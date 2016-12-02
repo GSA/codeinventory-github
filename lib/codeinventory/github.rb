0160b37aff5f2d53c3c55a8671e131ce672aad54
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2016-12-02 11:34:32 -0800

    remove old commented out code

--- lib/codeinventory/github.rb
+++ lib/codeinventory/github.rb
@@ -51,26 +51,6 @@ module CodeInventory
       metadata || {}
     end
 
-    # def projects
-    #   repos = client.organization_repositories(@org)
-    #   projects = []
-    #   repos.each do |repo|
-    #     repo_contents = client.contents(repo[:full_name], path: "/")
-    #     filenames = [ ".codeinventory.yml", "codeinventory.yml", ".codeinventory.json", "codeinventory.json"]
-    #     inventory_file = repo_contents.select { |file| filenames.include? file[:name] }.first
-    #     unless inventory_file.nil?
-    #       file_content = client.contents(repo[:full_name], path: inventory_file[:path])
-    #       raw_content = Base64.decode64(file_content[:content])
-    #       if inventory_file[:name].end_with? ".yml"
-    #         projects << YAML.load(raw_content).to_hash
-    #       elsif inventory_file[:name].end_with? ".json"
-    #         projects << JSON.parse(raw_content)
-    #       end
-    #     end
-    #   end
-    #   projects
-    # end
-
     # Provides a value for the name field.
     # Order of precedence:
     # 1. CodeInventory metadata file
