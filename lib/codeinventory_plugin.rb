d2d4ca48c9f06b6b244054151301be45e3c57989
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2017-02-03 12:43:49 -0500

    proper format for aliases

--- lib/codeinventory_plugin.rb
+++ lib/codeinventory_plugin.rb
@@ -5,8 +5,8 @@ module CodeInventory
   module CLI
     class App < Thor
       desc "github GITHUB_ACCESS_TOKEN GITHUB_ORG [OPTIONS]", "Build an inventory from GitHub"
-      option :overrides, aliases: :o, type: :hash, default: {}
-      option :exclude, aliases: :e, type: :array, default: []
+      option :overrides, aliases: "-o", type: :hash, default: {}
+      option :exclude, aliases: "-e", type: :array, default: []
       def github(access_token, org)
         source = CodeInventory::GitHub.new({ access_token: access_token }, org, overrides: options[:overrides], exclude: options[:exclude])
         inventory = CodeInventory::Inventory.new(source)
